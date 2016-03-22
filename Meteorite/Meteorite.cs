241e6a1faa2d6497bc48983d4ae2e7e859fc698c
JLitman70 <johnlitman307@gmail.com>
2016-03-21 23:25:40 -0500

    Added the Meteorite Project, Class Library and Class. Added the Intellitest Unit test for the constructor.

---
+++ Meteorite/Meteorite.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Meteorite
+{
+    public class Meteorite
+    {
+        //members right here
+        private string name;
+        private int id;
+        private int mass;
+        private string reclass;
+        private string fall;
+        private DateTime date;
+        private double rectLat;
+        private double rectLong;
+        //needs a geo location
+
+
+        //read only properties here
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+
+        }
+
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+
+
+        }
+
+        public int Mass
+        {
+            get
+            {
+                return mass;
+            }
+
+
+        }
+
+        public string Reclass
+        {
+            get
+            {
+                return reclass;
+            }
+
+
+        }
+
+        public string Fall
+        {
+            get
+            {
+                return fall;
+            }
+
+        }
+
+        public DateTime Date
+        {
+            get
+            {
+                return date;
+            }
+
+
+        }
+
+        public double RectLat
+        {
+            get
+            {
+                return rectLat;
+            }
+
+
+        }
+
+        public double RectLong
+        {
+            get
+            {
+                return rectLong;
+            }
+
+
+        }
+
+        //geo location object here
+
+
+        public Meteorite(string tname, int tid, int tmass, string treclass, string tfall, DateTime tdate, double tlat, double tlong)
+        {
+            name = tname;
+            id = tid;
+            mass = tmass;
+            reclass = treclass;
+            fall = tfall;
+            date = tdate;
+            rectLat = tlat;
+            rectLong = tlong;
+        }
+
+
+    }
+}
+
---
+++ Meteorite/Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Meteorite")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Meteorite")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("05bf07a9-3520-428b-84bf-ae04e0eece26")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: InternalsVisibleTo("Meteorite.Tests")]
+[assembly: InternalsVisibleTo("Meteorite.Explorables")]
+
