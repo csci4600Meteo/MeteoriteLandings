3b917ad68f1255b3d66b64e6f80e962a6995e377
Jason Payne <jpayne12@my.apsu.edu>
2016-03-28 22:00:06 -0500

    added additional show/hide functions for each vix subtype

--- Viz/MapVizContainer.cs
+++ Viz/MapVizContainer.cs
@@ -21,6 +21,69 @@ namespace Viz
             showPolys = true;
             showPushpins = true;
         }
+
+        public void hideMapAnnotations(Map map)
+        {
+            foreach(KeyValuePair<Guid,IMapViz> kvp in this)
+            {
+                if(kvp.Value is AnnoVizPoly && !showAnnotations)
+                {
+                    map.Children.Remove((AnnoVizPoly)kvp.Value);
+                }
+                else if (kvp.Value is AnnoVizPushpin && !showAnnotations)
+                {
+                    map.Children.Remove((AnnoVizPushpin)kvp.Value);
+                }
+            }
+        }
+
+        public void showMapAnnotations(Map map)
+        {
+            foreach (KeyValuePair<Guid, IMapViz> kvp in this)
+            {
+                if (kvp.Value is AnnoVizPoly && showAnnotations)
+                {
+                    map.Children.Add((AnnoVizPoly)kvp.Value);
+                }
+                else if (kvp.Value is AnnoVizPushpin && showAnnotations)
+                {
+                    map.Children.Add((AnnoVizPushpin)kvp.Value);
+                }
+            }
+        }
+
+
+        public void showMapMeteorites(Map map)
+        {
+            foreach (KeyValuePair<Guid, IMapViz> kvp in this)
+            {
+                if (kvp.Value is MeteoVizPoly && showMeteors)
+                {
+                    map.Children.Add((MeteoVizPoly)kvp.Value);
+                }
+                else if (kvp.Value is MeteoVizPushpin && showMeteors)
+                {
+                    map.Children.Add((MeteoVizPushpin)kvp.Value);
+                }
+            }
+        }
+
+        public void hideMapMeteorites(Map map)
+        {
+            foreach (KeyValuePair<Guid, IMapViz> kvp in this)
+            {
+                if (kvp.Value is MeteoVizPoly && !showMeteors)
+                {
+                    map.Children.Remove((MeteoVizPoly)kvp.Value);
+                }
+                else if (kvp.Value is MeteoVizPushpin && !showMeteors)
+                {
+                    map.Children.Remove((MeteoVizPushpin)kvp.Value);
+                }
+            }
+        }
+
+
         public void updateMap(Map map)
         {
             map.Children.Clear();
