10104c1ddcf3ee2d01ac483213be1348d2a69434
JLitman70 <johnlitman307@gmail.com>
2016-04-06 17:40:03 -0500

    Started coding the adapter need to research more

--- Meteorite/Meteorites.cs
+++ Meteorite/Meteorites.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Linq;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,10 +10,37 @@ namespace MeteoriteLib
 {
     class Meteorites : DataContext
     {
-        public Meteorites (string connectionString) : base(connectionString)
+        public Meteorites(string connectionString) : base(connectionString)
         {
-            
+            using (SqlConnection connection =
+                new SqlConnection(connectionString))  {
+                
+                //the sql adapter is created here
+                SqlDataAdapter adapter = new SqlDataAdapter();
+                //here i am mapping a "Meteorites" table within the adapter to the MeteoTable
+                adapter.TableMappings.Add("Meteorites", "MeteoTable");
+                //here i am going to open the connection
+                connection.Open();
+                //this is going to create a command copy everything from the database to the table
+                SqlCommand command = new SqlCommand("SELECT * from dbo.Meteorites");
+                // seems to be the way to tell it this command type is a string?
+                command.CommandType = System.Data.CommandType.Text;
+
+                //loads the adapter with the command we made
+                adapter.SelectCommand = command;
+
+                //not exactly sure how to do this so or what to put in
+           
+               //adapter.Fill();
+
+                //This is where i got, basically gonna wait til the database is up so
+                //i can make sure this is right first
+
+
+            }
+
         }
         public Table<Meteorite> MeteoTable;
+  
     }
 }
